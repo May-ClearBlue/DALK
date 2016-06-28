fa6a9b2f73dda7f2e52345114a49ba02587f9a82
May-ClearBlue <saya.swatty@gmail.com>
2016-06-28 20:35:24 +0900

    NDK周り

--- Dalk/Android/app/src/main/java/com/cyberneticcute/may_clearblue/dalk/MainActivity.java
+++ Dalk/Android/app/src/main/java/com/cyberneticcute/may_clearblue/dalk/MainActivity.java
@@ -1,13 +1,37 @@
-package com.cyberneticcute.may_clearblue.dalk;
+package com.CyberneticCute.Dalk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    static {
+        System.loadLibrary("native");
     }
+
+    GL2JNIView mView;
+    public native String getMsgFromJni();
+    /**
+     * @param width the current view width
+     * @param height the current view height
+     */
+    public static native void init(int width, int height);
+    public static native void step();
+
+    @Override protected void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        mView = new GL2JNIView(getApplication());
+        setContentView(mView);
+    }
+
+    @Override protected void onPause() {
+        super.onPause();
+        mView.onPause();
+    }
+
+    @Override protected void onResume() {
+        super.onResume();
+        mView.onResume();
+    }
+
+
 }
